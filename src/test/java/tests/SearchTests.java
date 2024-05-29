3426642a5a3a35046c6e36e8a5482bd6895109c2
u_m1y40 <dgkuzmichev@alfabank.ru>
2024-05-29 14:39:14 +0300

    - Add ios test config

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -1,13 +1,18 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class SearchTests extends TestBase {
@@ -21,6 +26,31 @@ public class SearchTests extends TestBase {
         step("Verify content found", () ->
                 $$(id("org.wikipedia.alpha:id/page_list_item_title"))
                         .shouldHave(sizeGreaterThan(0)));
+    }
+
+    @DisplayName("Checking com.browserstack.Sample")
+    @Test
+    @Tag("iOS")
+    void iosTextTest() {
+
+        String text = "iPhoneTest";
+        step("Click Text Button", () -> {
+            $(id("Text Button")).click();
+        });
+
+        step("Check initial state Output text", () -> {
+            assertEquals("Waiting for text input.", $(id("Text Output")).getText());
+        });
+
+        step(format("Set value %s in the input field and press enter", text), () -> {
+            $(id("Text Input")).click();
+            $(id("Text Input")).sendKeys(text);
+            $(id("Text Input")).pressEnter();
+        });
+
+        step("Check Output text", () -> {
+            $(accessibilityId("Text Output")).shouldHave(text(text));
+        });
     }
 
 }
